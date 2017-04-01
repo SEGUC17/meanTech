b63263ba719806fe093312eb67ae2d4b5da83a64
NardineSanad <nardine.sanad@outlook.com>
2017-04-01 21:19:04 +0200

    first co

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-=======
+
 var companyController = require('./controllers/companyController');
 var eventController = require('./controllers/eventController');
+var FAQController = require ('./controllers/FAQController');
 
 
+router.get('/company/profile',companyController.viewCompanyProfile);
 
 router.post('/company', companyController.companySubscription);
 router.post('/event', eventController.createEvent);
+router.post('/faq',FAQController.askFAQ);
 
 
 
 
 
->>>>>>> 1a09b8ee247820535400dc3e7a1f2bff5c986c2d
 
 module.exports = router;
