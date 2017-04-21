5fbc39b6cc4a98b064e31cc93b89dad4bab1dac8
mahmoud-abdalla <mahmoud95yasser@live.com>
2017-04-21 11:33:37 +0200

    om el error

--- public/js/controllers/admin.js
+++ public/js/controllers/admin.js
@@ -1,20 +1,13 @@
 const adminController = function ($scope, $location, factory) {
     $scope.formData = {};
 
-
     $scope.adminRegister = function adminRegister() {
-
-
-
         factory.adminRegister($scope.formData)
             .success(function (data) {
-                alert("Successfulley Registered! Please wait for an email on further details!")
+                alert("Now you can proceed to login.")
             }).error(function (error) {
                 alert(error.message);
             });
-
-
-
     }
 
 }
