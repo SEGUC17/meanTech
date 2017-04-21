b8bf009b686e57574c87745010af1cb4bef66915
Daniel Yanni <daniel.yanni@student.guc.edu.eg>
2017-04-22 00:02:16 +0200

    Search functionality done for view company events and viewRatings

--- public/js/controllers/event.js
+++ public/js/controllers/event.js
@@ -1,54 +1,55 @@
 const eventController = function ($scope, $location, factory) {
   $scope.eventForm = {};
-  $scope.updateForm={};
+  $scope.updateForm = {};
 
-    $scope.createEvent = function createEvent() {
-
-
-
-            factory.createEvent($scope.eventForm)
-              .success(function(data) {
-
-              alert("Event Successfully created!");
-
-            }).error(function(error) {
-
-              alert(error.message);
-
-            });
+  $scope.createEvent = function createEvent() {
 
 
 
-    };
+    factory.createEvent($scope.eventForm)
+      .success(function (data) {
 
+        alert("Event Successfully created!");
 
-     factory.getCompanyEvents().success(function(data){
-          console.log("it gets to the controller");
-          console.log(data);
+      }).error(function (error) {
 
-          
+        alert(error.message);
 
-          $scope.events=data.data;
-        });
-
-    $scope.updateEvent = function updateEvent() {
+      });
 
 
 
-            factory.updateEvent($scope.eventForm)
-              .success(function(data) {
-
-              alert("Event Successfully Updated!");
-
-            }).error(function(error) {
-
-              alert(error.message);
-
-            });
+  };
 
 
+  factory.getCompanyEvents().success(function (data) {
+    console.log("it gets to the controller");
+    console.log(data);
 
-    };
+    $scope.sortType = 'name'; // set the default sort type
+    $scope.sortReverse = false; // set the default sort order
+    $scope.searchFish = ''; // set the default search/filter term
+    $scope.events = data.data;
+  });
+
+  $scope.updateEvent = function updateEvent() {
+
+
+
+    factory.updateEvent($scope.eventForm)
+      .success(function (data) {
+
+        alert("Event Successfully Updated!");
+
+      }).error(function (error) {
+
+        alert(error.message);
+
+      });
+
+
+
+  };
 
 
 
