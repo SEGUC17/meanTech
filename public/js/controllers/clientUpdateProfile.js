d832a8941b6ebdab51be5192e4b9ed2696a735de
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-22 22:23:11 +0200

    mireille part

--- public/js/controllers/clientUpdateProfile.js
+++ public/js/controllers/clientUpdateProfile.js
@@ -1,15 +1,38 @@
 const updateProfileController = function ($scope, $location, factory) {
     $scope.clientUpdateProfileForm = {};
-   
-    $scope.clientUpdateProfile = function clientUpdateProfile() {
-        factory.clientUpdateProfile($scope.clientUpdatePasswordForm)
 
+    $scope.clientUpdateProfile = function clientUpdateProfile() {
+        factory.clientUpdateProfile($scope.clientUpdateProfileForm)
             .success(function (data) {
-                alert('Profile updated Successfully');
+                // alert('Profile updated Successfully');
+                console.log('SUCCESS =>', data);
             }).error(function (error) {
-                alert(error.message);
+                // alert(error.message);
+                console.log('ERROR =>', error);
             });
     };
 };
 updateProfileController.$inject = ['$scope', '$location', 'factory'];
 App.controller('updateProfileController', updateProfileController);
+
+
+// const updateProfileController = function ($scope, $location, factory) {
+
+
+//     factory.clientUpdateProfile()
+//         .then(function (data) {
+//             // console.log("in clientcieprofile.js");
+//             // console.log("in clientcieprofile.00js");
+//             // console.log(data.data[0]);
+//             // console.log("inisde the ctrl");
+//             // console.log(data.data.data[0])
+//             $scope.profile = data.data.data[0];
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// };
+
+// updateProfileController.$inject = ['$scope', '$location', 'factory'];
+// App.controller('updateProfileController', updateProfileController);
+
--- public/js/controllers/clientViewProfile.js
+++ public/js/controllers/clientViewProfile.js
@@ -1,20 +1,27 @@
 const viewProfileController = function ($scope, $location, factory) {
-   
-
-        factory.viewProfile()
-            .success(function (data) {
-                console.log("in clientcieprofile.js");
-                console.log(data);
-                            console.log("inisde the ctrl");
 
 
-                $scope.profile = data.data;
+    factory.viewProfile()
+        .then(function (data) {
+            // console.log("in clientcieprofile.js");
+            // console.log("in clientcieprofile.00js");
+            // console.log(data.data[0]);
+            // console.log("inisde the ctrl");
+            // console.log(data.data.data[0])
+            $scope.profile = data.data.data[0];
 
+            // DEMO, REMOVE
+            $scope.profile.favCompanies = ['Google', 'Microsoft'];
+            // ^ DEMO, REMOVE
 
-            });
-    };
-    
-    viewProfileController.$inject = ['$scope', '$location', 'factory'];
-    App.controller('viewProfileController', viewProfileController);
+            console.log(data.data.data[0]);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+viewProfileController.$inject = ['$scope', '$location', 'factory'];
+App.controller('viewProfileController', viewProfileController);
 
 
