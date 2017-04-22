c5d80172b7c64683c20e7066f4791fc4bb98ac02
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-22 12:13:13 +0200

    userViewPromotion workinggit add .git add .git add .

--- public/js/controllers/userViewAllPromotions.js
+++ public/js/controllers/userViewAllPromotions.js
@@ -1,20 +1,15 @@
 const userViewAllPromotionsController = function ($scope, $location, factory) {
-    $scope.viewPromotionsForm = {};
 
-    $scope.userViewAllPromotions = function userViewAllPromotions() {
 
-        factory.userViewAllPromotions() // there was $scope.viewPromotionsForm in paramater
-      .success(function (data) {
-          alert("You're viewing all the prmomotions"); // to be removed
-      }).error(function (error) {
-          alert(error.message);
-      });
+    factory.userViewAllPromotions() // there was $scope.viewPromotionsForm in paramater
+        .success(function (data) {
+            console.log("in.js");
+            $scope.promotions = data.data;
+        })
 
     //     factory.userViewAllPromotions().then((res)) => {
     //   const { data: { data: { collection: promotions}}} = res;
-  };
 };
-
 
 userViewAllPromotionsController.$inject = ['$scope', '$location', 'factory'];
 App.controller('userViewAllPromotionsController', userViewAllPromotionsController);
