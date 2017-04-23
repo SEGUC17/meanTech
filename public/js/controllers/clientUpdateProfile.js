083c73b39161a91ecc0a6b4594f14e221a7a7ed5
{Mireille} <{mireille.adel@yahoo.com}>
2017-04-23 21:20:12 +0200

    update and view done

--- public/js/controllers/clientUpdateProfile.js
+++ public/js/controllers/clientUpdateProfile.js
@@ -4,11 +4,9 @@ const updateProfileController = function ($scope, $location, factory) {
     $scope.clientUpdateProfile = function clientUpdateProfile() {
         factory.clientUpdateProfile($scope.clientUpdateProfileForm)
             .success(function (data) {
-                // alert('Profile updated Successfully');
-                console.log('SUCCESS =>', data);
+               // console.log('SUCCESS =>', data);
             }).error(function (error) {
-                // alert(error.message);
-                console.log('ERROR =>', error);
+               // console.log('ERROR =>', error);
             });
     };
 };
@@ -16,23 +14,4 @@ updateProfileController.$inject = ['$scope', '$location', 'factory'];
 App.controller('updateProfileController', updateProfileController);
 
 
-// const updateProfileController = function ($scope, $location, factory) {
-
-
-//     factory.clientUpdateProfile()
-//         .then(function (data) {
-//             // console.log("in clientcieprofile.js");
-//             // console.log("in clientcieprofile.00js");
-//             // console.log(data.data[0]);
-//             // console.log("inisde the ctrl");
-//             // console.log(data.data.data[0])
-//             $scope.profile = data.data.data[0];
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-// };
-
-// updateProfileController.$inject = ['$scope', '$location', 'factory'];
-// App.controller('updateProfileController', updateProfileController);
 
--- public/js/controllers/clientViewProfile.js
+++ public/js/controllers/clientViewProfile.js
@@ -3,16 +3,7 @@ const viewProfileController = function ($scope, $location, factory) {
 
     factory.viewProfile()
         .then(function (data) {
-            // console.log("in clientcieprofile.js");
-            // console.log("in clientcieprofile.00js");
-            // console.log(data.data[0]);
-            // console.log("inisde the ctrl");
-            // console.log(data.data.data[0])
             $scope.profile = data.data.data[0];
-
-            // DEMO, REMOVE
-            $scope.profile.favCompanies = ['Google', 'Microsoft'];
-            // ^ DEMO, REMOVE
 
             console.log(data.data.data[0]);
         })
