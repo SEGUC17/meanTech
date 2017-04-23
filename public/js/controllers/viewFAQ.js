7983454feee1c77f5bc1dae36acb9665953de578
mahmoud-abdalla <mahmoud95yasser@live.com>
2017-04-23 21:52:12 +0200

    hanet

---
+++ public/js/controllers/viewFAQ.js
@@ -0,0 +1,20 @@
+const FAQController = function ($scope, $location, factory) {
+
+        factory.viewFAQs()
+                .then(function (response) {
+                        $scope.faqs = response.data.data;
+                })
+                .catch(function (response) {
+
+                });
+
+        $scope.questionId = function questionId(id) {
+                $scope.id = {
+                        id
+                };
+                $location.path('/faqa');
+        };
+};
+
+FAQController.$inject = ['$scope', '$location', 'factory'];
+App.controller('FAQController', FAQController);
