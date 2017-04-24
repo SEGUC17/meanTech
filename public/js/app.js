d5b802503c6d89d5cd9ac0f4bcdce5aac8cc4245
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-24 02:15:49 +0200

    app.js forgotten concerning Naders merging

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 App = angular
 	.module('meanTech', ['ngRoute', 'angular-stripe'])
 	.config(function (stripeProvider) {
 		stripeProvider.setPublishableKey('pk_test_DBMKRqel64aYUUKBgAOOPjKe');
 	});
 
-=======
-var App= angular.module("meanTech", ['ngRoute']);
->>>>>>> 5162d400ce1355686fb59cddfbbde4c53e2d37b5
