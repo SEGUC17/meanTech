a2f3ebedb52f08f0de1a72a78376b3976b441ab6
NardineSanad <nardine.sanad@outlook.com>
2017-04-24 18:03:36 +0200

    Commenting commit

--- public/js/controllers/review.js
+++ public/js/controllers/review.js
@@ -1,9 +1,9 @@
 const reviewController = function ($scope, $location, factory) {
    
     const company = factory.getCompanyReview();
+    // function that views the reviews of the company to anyone
     factory.viewRatings(company).success(function (data) {
-        console.log('it gets to the review controller');
-        console.log(data);
+    
         $scope.sortType = 'name'; // set the default sort type
         $scope.sortReverse = false; // set the default sort order
         $scope.searchFish = ''; // set the default search/filter term
