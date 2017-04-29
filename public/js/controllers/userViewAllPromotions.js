891c0ba90830938677b9bfd6353c1d87b8a380b3
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-30 00:03:11 +0200

    some changes

--- public/js/controllers/userViewAllPromotions.js
+++ public/js/controllers/userViewAllPromotions.js
@@ -1,5 +1,9 @@
 const userViewAllPromotionsController = function ($scope, $location, factory) {
 
+ $scope.constructDate = function (dateString) {
+      return new Date(dateString);
+    };
+    
 //to let a visitor and a client see all the promotions
     factory.userViewAllPromotions()
         .success(function (data) {
