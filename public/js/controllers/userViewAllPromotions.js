6560994a0577be2c5d8d730beab4d300a633b223
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-29 11:57:42 +0200

    tmp promotions expiry date

--- public/js/controllers/userViewAllPromotions.js
+++ public/js/controllers/userViewAllPromotions.js
@@ -1,11 +1,22 @@
 const userViewAllPromotionsController = function ($scope, $location, factory) {
 
-//to let a visitor and a client see all the promotions
+    //to let a visitor and a client see all the promotions
     factory.userViewAllPromotions()
         .success(function (data) {
-       
-            $scope.promotions = data.data;
+
+            // $scope.promotions = data.data;
+            $scope.promotions = data.data.map(function (x) {
+                x.expiry = new Date(x.expiry);
+                return x;
+            });
+
         })
+
+    // .map(function(x){
+    //     x.expiry = new Date(x.expiry);
+    //     return x;
+    // });
+
 };
 
 userViewAllPromotionsController.$inject = ['$scope', '$location', 'factory'];
