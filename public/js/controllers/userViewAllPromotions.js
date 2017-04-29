71ab5474502ca91ab65dfd50dc3727e995d07022
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-29 20:25:45 +0200

    tmp

--- public/js/controllers/userViewAllPromotions.js
+++ public/js/controllers/userViewAllPromotions.js
@@ -1,10 +1,14 @@
 const userViewAllPromotionsController = function ($scope, $location, factory) {
 
-//to let a visitor and a client see all the promotions
+    //to let a visitor and a client see all the promotions
     factory.userViewAllPromotions()
         .success(function (data) {
-       
-            $scope.promotions = data.data;
+
+            //$scope.promotions = data.data;
+            $scope.promotions = response.promotions.map(function (element) {
+                element.date = new Date(element.date);
+                return element;
+            });
         })
 };
 
