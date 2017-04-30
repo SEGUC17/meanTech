6e6e408813248ac7d9af6a02e17c9336e990dbc0
Syrine Hamza <syrine.hamza@student.guc.edu.eg>
2017-04-30 04:21:26 +0200

    date when view promotions

--- public/js/controllers/userViewAllPromotions.js
+++ public/js/controllers/userViewAllPromotions.js
@@ -1,9 +1,11 @@
 const userViewAllPromotionsController = function ($scope, $location, factory) {
-
-//to let a visitor and a client see all the promotions
+    $scope.constructDate = function (dateString) {
+        return new Date(dateString);
+    };
+    //to let a visitor and a client see all the promotions
     factory.userViewAllPromotions()
         .success(function (data) {
-       
+
             $scope.promotions = data.data;
         })
 };
